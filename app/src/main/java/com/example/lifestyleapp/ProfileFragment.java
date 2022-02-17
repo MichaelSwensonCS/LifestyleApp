ba096d5b5e5bc4495fc85be7e8c89156ffc185fa
robertche54 <u1119412@utah.edu>
2022-02-17 16:17:06 -0700

    Added basic fragment switching for testing

--- app/src/main/java/com/example/lifestyleapp/ProfileFragment.java
+++ app/src/main/java/com/example/lifestyleapp/ProfileFragment.java
@@ -1,7 +1,6 @@
 package com.example.lifestyleapp;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -12,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -22,8 +20,12 @@ import androidx.appcompat.widget.AppCompatButton;
 import androidx.annotation.Nullable;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.lifestyleapp.databinding.FragmentFirstBinding;
+import com.example.lifestyleapp.databinding.FragmentProfilePageBinding;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.io.File;
@@ -52,7 +54,6 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
     static final int REQUEST_IMAGE_CAPTURE = 1;
     Intent mDisplayIntent;
 
-
     private TextInputEditText name_first;
     private TextInputEditText name_last;
     private AutoCompleteTextView age;
@@ -62,33 +63,40 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
     private AutoCompleteTextView height_feet;
     private AutoCompleteTextView height_inch;
 
+    private static User user;
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
         binding = FragmentFirstBinding.inflate(inflater, container, false);
-
         return binding.getRoot();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        boolean init = false;
+        // Create user
+        if (user == null) {
+            user = new User();
+            init = true;
+            // The radio buttons are on male by default
+            user.gender = "male";
+        }
 
         AppCompatButton submitButton =(AppCompatButton) getActivity().findViewById(R.id.submitBtn);
         submitButton.setOnClickListener(this);
 
-
         //This might be a problem
         mDisplayIntent = new Intent(getActivity(),view.getClass());
 
-
-        //This was needed to populate the dropdown menu(Spinner age)
-        List age = new ArrayList<Integer>();
         // Name
         name_first = getActivity().findViewById(R.id.userFirstName);
+        name_first.setText(init ? "" : user.firstname);
         name_last = getActivity().findViewById(R.id.userLastName);
+        name_last.setText(init ? "" : user.lastname);
 
         // Age
         List ages = new ArrayList<Integer>();
@@ -98,22 +106,26 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
         ArrayAdapter<Integer> agesAdapter = new ArrayAdapter<Integer>(getActivity(), android.R.layout.simple_spinner_item, ages);
         agesAdapter.setDropDownViewResource( android.R.layout.simple_spinner_item );
 
-
+        age = getActivity().findViewById(R.id.ageDropDown);
+        age.setText(init || user.age < 0 ? "" : "" + user.age);
+        age.setAdapter(agesAdapter);
 
         // Weight
         weight = getActivity().findViewById(R.id.userWeight);
+        weight.setText(init || user.weight < 0 ? "" : "" + user.weight);
 
         //City autocomplete
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_list_item_1, CITIES);
-        AutoCompleteTextView textView = (AutoCompleteTextView)
-                getActivity().findViewById(R.id.cityAuto);
-        textView.setAdapter(adapter);
+        city = getActivity().findViewById(R.id.cityAuto);
+        city.setText(init ? "" : user.city);
+        city.setAdapter(adapter);
 
         // Country
         ArrayAdapter<String> countriesAdapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_list_item_1, COUNTRIES);
         country = getActivity().findViewById(R.id.countryAuto);
+        country.setText(init ? "" : user.country);
         country.setAdapter(countriesAdapter);
 
         // Height
@@ -125,6 +137,7 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
         feetAdapter.setDropDownViewResource( android.R.layout.simple_spinner_item );
 
         height_feet = getActivity().findViewById(R.id.feetAuto);
+        height_feet.setText(init || user.heightfeet < 0 ? "" : "" + user.heightfeet);
         height_feet.setAdapter(feetAdapter);
 
         List heights_inch = new ArrayList<Integer>();
@@ -135,22 +148,10 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
         inchAdapter.setDropDownViewResource( android.R.layout.simple_spinner_item );
 
         height_inch = getActivity().findViewById(R.id.inchesAuto);
+        height_inch.setText(init || user.heightinches < 0 ? "" : "" + user.heightinches);
         height_inch.setAdapter(inchAdapter);
 
         // Sex
-        Context context = getContext();
-        CharSequence text = "Female Selected";
-        int duration = Toast.LENGTH_SHORT;
-        Toast toast = Toast.makeText(context,text,duration);
-
-        // Bind click listener to buttons
-        AppCompatButton button = getActivity().findViewById(R.id.uploadPicture);
-        button.setOnClickListener(this);
-
-        button = getActivity().findViewById(R.id.submitBtn);
-        button.setOnClickListener(this);
-
-
         RadioGroup radioGroup = getActivity().findViewById(R.id.radioSexGroup);
         binding.radioSexGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
         {
@@ -158,14 +159,21 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 switch(checkedId){
                     case R.id.radioFemale:
-                        toast.show();
+                        user.gender = "female";
                         break;
                     case R.id.radioMale:
-                    default:
+                        user.gender = "male";
                         break;
                 }
             }
         });
+
+        // Bind click listener to buttons
+        AppCompatButton button = getActivity().findViewById(R.id.uploadPicture);
+        button.setOnClickListener(this);
+
+        button = getActivity().findViewById(R.id.submitBtn);
+        button.setOnClickListener(this);
     }
 
     @Override
@@ -174,37 +182,109 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
         binding = null;
     }
 
+    private void showToast(String msg, int duration) {
+        Toast toast = Toast.makeText(getContext(), msg, duration);
+        toast.show();
+    }
+
+    private boolean saveInfo(boolean validate) {
+        // Pull name
+        if (!validate || !name_first.getText().toString().isEmpty()) user.firstname = name_first.getText().toString();
+        else {
+            showToast("First name cannot be empty", Toast.LENGTH_SHORT);
+            return false;
+        }
+        if (!validate || !name_last.getText().toString().isEmpty()) user.lastname = name_last.getText().toString();
+        else {
+            showToast("Last name cannot be empty", Toast.LENGTH_SHORT);
+            return false;
+        }
+
+        // Parse age
+        if (age.getText().toString().isEmpty()) user.age = -1;
+        else try {
+            int years = Integer.parseInt(age.getText().toString());
+            if (years < 0) throw new NumberFormatException();
+            user.age = years;
+        }
+        catch (final NumberFormatException e) {
+            if (!validate) user.age = -1;
+            else {
+                showToast("Invalid age", Toast.LENGTH_SHORT);
+                return false;
+            }
+        }
+
+        // Parse weight
+        if (weight.getText().toString().isEmpty()) user.weight = -1;
+        else try {
+            int pounds = Integer.parseInt(weight.getText().toString());
+            if (pounds < 0) throw new NumberFormatException();
+            user.weight = pounds;
+        }
+        catch (final NumberFormatException e) {
+            if (!validate) user.weight = -1;
+            else {
+                showToast("Invalid weight", Toast.LENGTH_SHORT);
+                return false;
+            }
+        }
+
+        // Pull city and country
+        user.country = country.getText().toString();
+        user.city = city.getText().toString();
+
+        // Parse height
+        if (height_feet.getText().toString().isEmpty()) user.heightfeet = -1;
+        else try {
+            int height = Integer.parseInt(height_feet.getText().toString());
+            if (height < 0) throw new NumberFormatException();
+            user.heightfeet = height;
+        }
+        catch (final NumberFormatException e) {
+            if (!validate) user.heightfeet = -1;
+            else {
+                showToast("Invalid height", Toast.LENGTH_SHORT);
+                return false;
+            }
+        }
+
+        if (height_inch.getText().toString().isEmpty()) user.heightinches = -1;
+        else try {
+            int height = Integer.parseInt(height_inch.getText().toString());
+            if (height < 0) throw new NumberFormatException();
+            user.heightinches = height;
+        }
+        catch (final NumberFormatException e) {
+            if (!validate) user.heightinches = -1;
+            else {
+                showToast("Invalid height", Toast.LENGTH_SHORT);
+                return false;
+            }
+        }
+        return true;
+    }
+
     @Override
     public void onClick(View view) {
-        Context context = getContext();
-        CharSequence text = view.getId() + "";
-        int duration = Toast.LENGTH_SHORT;
-        Toast toast = Toast.makeText(context,text,duration);
-
-        toast.show();
         switch(view.getId()){
-            case R.id.radioFemale:
-                toast.show();
-                break;
-            case R.id.radioMale:
-                toast.show();
-                break;
 
             case R.id.submitBtn:
                 // BMI = 703 × pounds/(inches)^2
-                int pounds = Integer.parseInt(weight.getText().toString());
-                int feet = Integer.parseInt(height_feet.getText().toString());
-                int inches = Integer.parseInt(height_inch.getText().toString());
-                float bmi = (float) (703 * pounds/Math.pow(feet*12 + inches, 2));
-                Toast info = Toast.makeText(context, "BMI: " + bmi, duration);
-                info.show();
+                if (saveInfo(true)){
+                    // switch to another fragment
+                    NavHostFragment.findNavController(ProfileFragment.this)
+                            .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                }
                 break;
+
             case R.id.uploadPicture:
                 Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if(cameraIntent.resolveActivity(getActivity().getPackageManager())!=null){
                     startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
                 }
                 break;
+
             default:
                 break;
         }
@@ -223,7 +303,7 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
                 mDisplayIntent.putExtra("imagePath",filePathString);
                 ImageView profile = (ImageView) getActivity().findViewById(R.id.imageView);
                 profile.setImageBitmap(mThumbnailImage);
-
+                user.photo = mThumbnailImage;
             }
             else{
                 Toast.makeText(getActivity(),"External storage not writable.",Toast.LENGTH_SHORT).show();
@@ -233,7 +313,6 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
     }
 
     String currentPhotoPath;
-
     private String saveImage(Bitmap finalBitmap) {
         String APP_TAG = "LifestyleApp";
         String root = Environment.getExternalStorageDirectory().toString();
@@ -265,4 +344,5 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
         }
         return false;
     }
+
 }
