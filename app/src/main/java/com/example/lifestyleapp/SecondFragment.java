60647fbfb2084bfc0b97af57d6eecfd92a54a1f8
Mike <Felixscort@gmail.com>
2022-02-23 18:55:34 -0700

    Navigation stuff, still need to hook up more to profile/weather

--- app/src/main/java/com/example/lifestyleapp/SecondFragment.java
+++ app/src/main/java/com/example/lifestyleapp/SecondFragment.java
@@ -10,14 +10,15 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.lifestyleapp.databinding.FragmentSecondBinding;
+import com.example.lifestyleapp.databinding.FragmentFitnessPageBinding;
+import com.example.lifestyleapp.databinding.FragmentProfilePageBinding;
 
 public class SecondFragment extends Fragment implements View.OnClickListener {
     Button home;
     Button profile;
     Button submit;
 
-    private FragmentSecondBinding binding;
+    private FragmentFitnessPageBinding binding;
 
     @Override
     public View onCreateView(
@@ -25,33 +26,31 @@ public class SecondFragment extends Fragment implements View.OnClickListener {
             Bundle savedInstanceState
     ) {
 
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        binding = FragmentFitnessPageBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        home = (Button) getActivity().findViewById(R.id.btn_home);
+        home = (Button) getActivity().findViewById(R.id.btn_homeFitness);
         home.setOnClickListener(this);
 
-        profile = (Button) getActivity().findViewById(R.id.btn_Profile);
+        profile = (Button) getActivity().findViewById(R.id.btn_ProfileFitness);
         profile.setOnClickListener(this);
-
-
 
     }
         @Override
         public void onClick(View view) {
             switch(view.getId()){
 
-                case R.id.btn_home:
+                case R.id.btn_homeFitness:
                     NavHostFragment.findNavController(SecondFragment.this).navigate(R.id.action_fitness_to_home);
                     break;
-                case R.id.btn_FitReg:
-                    break;
-                case R.id.btn_Profile:
+
+                case R.id.btn_ProfileFitness:
                     NavHostFragment.findNavController(SecondFragment.this).navigate(R.id.action_FitnessReg_to_Profile);
+                    break;
                 case R.id.btn_CalcBMR:
                     //TODO CALC BMR
                     break;
